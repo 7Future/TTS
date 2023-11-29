497c8166ead7c5890f6d05440aa68a39ac510a97
Fear <96379040+7Future@users.noreply.github.com>
2023-11-29 22:31:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const countdownDate = new Date("2023-12-24T00:00:00").getTime();
+const countdownSound = document.getElementById('countdownSound');
+const timerDisplay = document.getElementById('timer');
+
+const timer = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (distance < 0) {
+    clearInterval(timer);
+    timerDisplay.innerHTML = '<button onclick="redirectToAnotherWebsite()" class="play-now-button">PLAY NOW</button>';
+    countdownSound.play();
+  } else {
+    // Display the countdown timer
+    timerDisplay.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    countdownSound.play();
+  }
+}, 1000);
+
+function redirectToAnotherWebsite() {
+  window.location.href = 'https://www.roblox.com/games/14816191294/WW2-Tank-Simulator-OSTFRONT-1944'; // Replace with your desired URL
+}
